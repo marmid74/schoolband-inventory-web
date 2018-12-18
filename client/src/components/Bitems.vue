653861dc791668ad191945c1766e590bb2c3439c
marmid74 <marmid74@gmail.com>
2018-12-18 09:41:16 +0100

    first step of login functionality

---
+++ client/src/components/Bitems.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>Brassband inventory items</h2>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/src/components/Login.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <h2>Login</h2>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-text-field
+          label="E-mail"
+          v-model="email"
+          v-bind:rules="emailRules"
+          required>
+        </v-text-field>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-text-field
+          label="Password"
+          v-model="password"
+          v-bind:rules="passwordRules"
+          v-bind:type="'password'"
+          required>
+        </v-text-field>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-btn v-on:click="cancel">Cancel</v-btn>
+        <v-btn v-on:click="login" color="primary">Login</v-btn>
+      </v-flex>     
+    </v-layout>
+    <v-snackbar
+      :timeout="6000"
+      :top="true"
+      v-model="showAlert"
+    >
+      {{ message }}
+    </v-snackbar>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      showAlert: false,
+      message: '',
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+      ],
+      password: '',
+      passwordRules: [
+        v => !!v || 'Password is required'
+      ]
+    }
+  },
+  methods: {
+    login: function () {
+      const vm = this
+      if (vm.password === 'test111') {
+        this.$router.push({path: '/'})
+      } else {
+        //  show alert to user
+        vm.showAlert = true
+        vm.message = 'email or password is invalid'
+      }
+    },
+    cancel: function (){
+      console.log('The user does not want to log in')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
