cea3af4a80c295d685b8a3e00f5d14591c0c719c
marmid74 <marmid74@gmail.com>
2019-02-08 14:37:09 +0100

    merge from master

--- src/controllers/asset.controller.js
+++ src/controllers/asset.controller.js
@@ -1,7 +1,72 @@
 import Asset from '../models/asset.model';
 
 
-// POST
+/* export function dummy(req,res){
+    return res.status(200).send({message: 'Yep, api alive'});
+}
+*/
+
+
+// Generic endpoints for asses objects
+
+export function findOne(req,res) {   
+    console.log("EndpointAPI findOne: ", req.body);
+    let filterObj =  req.body;
+    Asset.find(filterObj, (err, asset) => {
+        if(err) return res.status(500).send(err);         
+        return res.status(200).send(asset);
+    }); 
+}
+
+// PUT
+export function findByIdAndUpdate(req, res){
+    console.log("EndpointAPI: findByIdAndUpdate: ", req.body);
+    Asset.findByIdAndUpdate(req.params.itemId, req.body, {new:true}, (err, asset) => {
+        if(err) return res.status(500).send(err);
+        return res.status(200).send({'message': 'Successfully updated asset',
+                                     'inputdata': req.body,
+                                     'body': asset
+                                    });
+    })
+}
+
+// get - no body filter by spilling url for uniform/instrument/note
+export function getAll(req,res) {   
+    console.log("EndpointAPI getAll: ", req.body);
+    let filterObj = {
+        "assettype": req.url.split('/')[3]
+    }
+    
+    Asset.find(filterObj, (err, asset) => {
+        if(err) return res.status(500).send(err);         
+        return res.status(200).send(asset);
+    }); 
+}
+
+
+export function findById(req,res){
+    console.log("EndpointAPI: findById");
+    Asset.findById(req.params.itemId, (err, item) => {
+        if (err) return res.status(500).send(err);
+        return res.status(200).send(item);
+    });
+}
+
+
+
+export function findByIdAndDelete(req, res){
+    console.log("EndpointAPI: findByIdAndDelete");
+    Asset.findByIdAndDelete(req.params.itemId, (err, items) => {
+        if (err) return res.status(500).send(err);
+        const response = {
+            message: "Item successfully deleted",
+            id: items._id
+        };
+        return res.status(200).send(response);
+    });
+}
+
+// POST  CREATE UNIFORM
 export async function createUniform (req,res) {
     console.log("EndpointAPI: createUniform ", req.body.itemtype );
     let countItems = 0;
@@ -9,8 +74,6 @@ export async function createUniform (req,res) {
     console.log('body:', req.body);
     
     let filterObj = {           
-        // "uniform.itemtype": req.body.uniform.itemtype,
-        // "uniform.size": req.body.uniform.size
         "assettype":  req.body.assettype        
     };
     console.log('Searchobj: ', filterObj);
@@ -37,8 +100,9 @@ export async function createUniform (req,res) {
     }
 }
 
+
+// POST  CREATE INSTRUMENT
 export async function createInstrument (req,res) {
-    console.log("EndpointAPI: createInstrument ", req.body.itemtype );
     console.log('body:', req.body);
     
     let filterObj = {           
